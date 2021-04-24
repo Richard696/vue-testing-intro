fc9b486f9352eb1af3b6baff8d92cd881aead952
YeeHeng <lauyh87@gmail.com>
2021-04-25 00:18:34 +0800

    complete the test for login form

--- tests/unit/Loginfrom.spec.js
+++ tests/unit/Loginfrom.spec.js
@@ -1,1 +1,17 @@
 import LoginForm from '@/components/LoginForm'
+import {mount} from '@vue/test-utils'
+
+describe('LoginForm', ()=>{
+    it('emits an event with a user data payload', ()=>{
+        const wrapper = mount(LoginForm);
+        const loginForm = wrapper.find('input[type="text"]');
+        loginForm.setValue('Test Value');
+        wrapper.trigger('submit'); // Simulate form submission
+
+        // assert when the form is submitted
+        const formSubmittedCalls = wrapper.emitted('formSubmitted');
+        expect(formSubmittedCalls).toHaveLength(1)
+        const expectedPayload = {name: 'Test Value'}
+        expect(wrapper.emitted('formSubmitted')[0][0]).toMatchObject(expectedPayload)
+    })
+})
